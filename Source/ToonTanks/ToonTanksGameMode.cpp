cb2e49a3d6740c6416e0933d0489a6c84ebda487
CKirkby <conorkirkby1121@gmail.com>
2024-09-02 17:45:57 +0100

    Spawning Outside Map Fixed
    The turrets no longer spawn outside the map, if they try to they will be snapped back into the level at the closest point. I have set a manual bounds of the level.

--- Source/ToonTanks/ToonTanksGameMode.cpp
+++ Source/ToonTanks/ToonTanksGameMode.cpp
@@ -14,8 +14,6 @@ void AToonTanksGameMode::BeginPlay()
 	HandleGameStart();
 
 	SpawnTurret();
-
-	DrawDebugSphere(GetWorld(), Tank->GetActorLocation(), 750.f, 20, FColor::Red, true);
 }
 
 void AToonTanksGameMode::ActorDied(AActor* DeadActor)
@@ -73,21 +71,38 @@ void AToonTanksGameMode::HandleGameStart()
 
 FVector AToonTanksGameMode::CalculateSpawnArea(AActor* OriginActor, float MinDistance, float MaxDistance)
 {
+	// Calculates a random angle in radians (is 360 degrees)
 	float RandomAngle = FMath::RandRange(0.f, 2.0f * PI);
 	float RandomDistance = FMath::RandRange(MinDistance, MaxDistance);
-        
+
+	// Calculates a random offset in the x and y vectors by multiplying a cos and sin angle
 	float OffsetX = RandomDistance * FMath::Cos(RandomAngle);
 	float OffsetY =  RandomDistance * FMath::Sin(RandomAngle);
+
+	// Manually sets the map bounds
+	FVector2d MinMapBoundary = FVector2d(-2350.f, -2350.f);
+	FVector2d MaxMapBoundary = FVector2d(2350.f, 2350.f);
+
+	// Adds all the random variables together to get a random location within a radius and returns it 
 	RandomLocation = OriginActor->GetActorLocation() + FVector(OffsetX, OffsetY, 0);
 
+	/* If this is correct, should check if any parts of the spawn location are greater than the boundary I have set, if so it will clamp it to the boundary max
+	   and place it back into the level albeit at the edge rather then random*/
+	if (RandomLocation.X < MinMapBoundary.X || RandomLocation.X > MaxMapBoundary.X || RandomLocation.Y < MinMapBoundary.Y || RandomLocation.Y > MaxMapBoundary.Y)
+	{
+		RandomLocation.X = FMath::Clamp(RandomLocation.X, MinMapBoundary.X, MaxMapBoundary.X);
+		RandomLocation.Y = FMath::Clamp(RandomLocation.Y, MinMapBoundary.Y, MaxMapBoundary.Y);
+	}
+	
 	return RandomLocation;
 }
 
 void AToonTanksGameMode::SpawnTurret()
 {
+	// Gets the location from the random calculated location
 	FVector SpawnLocation = CalculateSpawnArea(Tank, MinRadius, MaxRadius);
 	
-	// Checks the tower is not a null and spawns.
+	// Checks the tower is not null, there is not more than needed and the activation bool is true.
 	if (TowerClass != nullptr && CurrentTowers < MaxTowers && TowerSpawnTimerActivate == true)
 	{
 		GetWorld()->SpawnActor<ATower>(TowerClass, SpawnLocation, FRotator::ZeroRotator);
